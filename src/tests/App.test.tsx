74f84fdfe8e577a55dbc4217383f88978a2d72c3
Claus Haas <claus.haas@me.com>
2023-10-15 10:50:35 -0300

    Inicia testes da aplicação

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,23 @@
-import { screen } from '@testing-library/react';
-
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { vi } from 'vitest';
+import * as APIModule from '../services/newsAPI';
 import App from '../App';
 import renderWithRouterAndRedux from '../utils/renderWithReduxAndRouter';
+import { newsMockData } from './mock/mockData';
 
-it('should show "Vite + React + Trybe" text', () => {
+beforeEach(() => {
+  vi.spyOn(APIModule, 'fetchNews').mockResolvedValue(newsMockData);
+});
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+it('should show "Vite + React + Trybe" text', async () => {
   renderWithRouterAndRedux(<App />);
-  expect(screen.getByText(/vite \+ react \+ trybe/i)).toBeInTheDocument();
+  expect(screen.getByText(/Carregando.../i)).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(screen.getByText(/Carregando.../i));
+
+  screen.debug();
 });
