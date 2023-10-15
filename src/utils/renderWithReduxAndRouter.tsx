5867ebcbe3cb49b5524cb1a2fe46f94de0d3aee0
Claus Haas <claus.haas@me.com>
2023-10-15 10:50:15 -0300

    corrige renderWithReduxAndRouter

--- src/utils/renderWithReduxAndRouter.tsx
+++ src/utils/renderWithReduxAndRouter.tsx
@@ -4,9 +4,11 @@ import userEvent from '@testing-library/user-event';
 import { applyMiddleware, combineReducers, legacy_createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { ThemeProvider } from 'styled-components';
 import { ReduxState } from '../types';
 import newsReducer from '../redux/reducers/newsReducer.ts';
 import favoritesReducer from '../redux/reducers/favoritesReducer.ts';
+import theme from '../assets/styledTheme.ts';
 
 function renderWithRouterAndRedux(
   component: JSX.Element,
@@ -25,9 +27,11 @@ function renderWithRouterAndRedux(
 
   return {
     ...render(
-      <BrowserRouter>
-        <Provider store={ store }>{component}</Provider>
-      </BrowserRouter>,
+      <ThemeProvider theme={ theme }>
+        <BrowserRouter>
+          <Provider store={ store }>{component}</Provider>
+        </BrowserRouter>
+      </ThemeProvider>,
     ),
     user: userEvent.setup(),
     store,
